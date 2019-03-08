7bc1f62b69ce4093826b5e621ba25b868dfea6c8
huisongxiaomanong <976226535@qq.com>
2019-03-08 11:50:36 +0800

    baocun

--- today/src/today/Study.java
+++ today/src/today/Study.java
@@ -1,7 +1,13 @@
 package today;
 
+import java.util.Base64.Decoder;
+
 public interface Study {
 	String study(String classs);
 	int trying();
 	De hehe(De d);
+	public static void main(String[] args) {
+		int a = 1;
+		De de = new De();
+	}
 }
